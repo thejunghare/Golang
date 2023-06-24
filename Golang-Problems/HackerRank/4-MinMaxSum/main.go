fddeec7ca0a867273b7088583d6e2e448e029db6
prasad-nimap <prasadjunghare@nimapinfotech.com>
2023-06-24 18:47:48 +0530

    added 04 problem hackerrank

--- Golang-Problems/HackerRank/4-MinMaxSum/main.go
+++ Golang-Problems/HackerRank/4-MinMaxSum/main.go
@@ -6,20 +6,22 @@ import "fmt"
 
 func main() {
 	arr := []int32{7, 69, 2, 221, 8974}
-	//	min, max := 0, 0
-	min := 0
+	min, max := 0, 0
+	//min := 0
 
 	// for min
-	for index := 1; index < (len(arr)); index++ {
+	for index := 0; index < (len(arr) - 1); index++ {
 		min += int(arr[index])
-		fmt.Println(int(arr[index]) + min)
+		//fmt.Println(int(arr[index]) + min)
 	}
-	fmt.Println(min)
+	//	fmt.Println(min)
 
 	// for max
-	/*for index := 2; index < (len(arr) + 1); index++ {
+	for index := 1; index < (len(arr)); index++ {
+		fmt.Println(max)
 		max = int(arr[index]) + max
+
 	}
 
-	fmt.Println(min, max)*/
+	fmt.Println(min, max)
 }
