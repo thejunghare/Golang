b20ba7f5ec663bf76adb92f7acc3906e68864a4e
prasad-nimap <prasadjunghare@nimapinfotech.com>
2023-06-27 12:46:24 +0530

    added question for gohercises>find_two_that_sum

--- Golang-Problems/Gophercises/find_two_that_sum.go
+++ Golang-Problems/Gophercises/find_two_that_sum.go
@@ -1,3 +1,11 @@
+/*
+Find two numbers in a list that sum to a given amount.
+
+FindTwoThatSum will look for two numbers in the provided numbers list that sum up to the sum argument. It will then return the indices of each of these numbers.
+
+If there are multiple correct answers, any of them may be used. If there are no correct answers, (-1, -1) should be returned.
+*/
+
 package main
 
 import "fmt"
@@ -15,17 +23,17 @@ func FindTwoThatSum(list []int, sum int) []int {
 }
 
 func FindTwoThatSumUsingRangeKey(list []int, sum int) (int, int) {
-	for i,val1 := range list {
+	for i, val1 := range list {
 		for j, val2 := range list {
 			if i == j {
 				continue
 			}
-			if val1 + val2 == sum {
-				return i , j
+			if val1+val2 == sum {
+				return i, j
 			}
 		}
 	}
-	
+
 	return -1, -1
 }
 
