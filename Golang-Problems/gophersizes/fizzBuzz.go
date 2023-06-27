de743477664309f5ecd7ece0f271a0a5edb0e004
prasad-nimap <prasadjunghare@nimapinfotech.com>
2023-06-27 11:45:39 +0530

    added gopersizes>FizzBuzz

--- Golang-Problems/gophersizes/fizzBuzz.go
+++ Golang-Problems/gophersizes/fizzBuzz.go
@@ -1,8 +1,25 @@
-/* 
+/*
 	Given a number N, print out all the numbers from 1 to N but when a number is divisible by 3 print out "Fizz" instead of the number, and when a number is divisible by 5 print out "Buzz" instead of the number. For numbers divisible by both 3 and 5 print "Fizz Buzz".
 */
 
 package main
 
-func FizzBuzz(num int)string{return "Fizz"}
-func main(){}
+import "fmt"
+
+func FizzBuzz(n int) {
+	for i := 1; i <= n; i++ {
+		if i%3 == 0 {
+			fmt.Println("Fizz")
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
+		} else if i%3 == 0 && i%5 == 0 {
+			fmt.Println("FizzBuzz")
+		} else {
+			fmt.Println(i)
+		}
+	}
+}
+func main() {
+	n := 100
+	FizzBuzz(n)
+}
