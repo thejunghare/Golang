dc71dec6899bd45c8ad2ed47365bd0c3da5ee6c3
prasad-nimap <prasadjunghare@nimapinfotech.com>
2023-06-29 15:41:33 +0530

    added readme.md and updated code for interfaces

---
+++ Golang-Tutorials/Interfaces/README.md
@@ -0,0 +1,9 @@
+# Interface
+
+Interfaces define a set of method signatures. An interface specifies what methods a type must have in order to be considered as implementing that interface.
+
+In simple words interface lets us group together different types based on their behaviour.
+
+Interfaces are like rule book which define define a method (properties) to be considered as a specific type.
+
+for example: type Human has properties like walk(), talk(), etc, so to be a type of Human you should implement the methods(properties) of type Human.
--- Golang-Tutorials/Interfaces/interfaces.go
+++ Golang-Tutorials/Interfaces/interfaces.go
@@ -1,69 +1,35 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
-/*
-	Interfaces: 1. Custom type used to specfiy a set of one or more method signatures
-				2. interfaces are abstract.
-				3. You are not allowed to create an instance of the interface.
-				4. Its necessary to implement all the methods declared in interface.
-
-	syntax: type interface_name interface {
-			method_name() string
-	}
-*/
-
-/* Interface for shape */
-type shape interface {
-	area() float64
-	circumf() float64
+type Animal interface {
+	Speak() string
+	Walk() string
 }
 
-/* struct square */
-type square struct {
-	length float64
+type Dog struct{}
+
+func (d Dog) Speak() string {
+	return "Woof"
 }
 
-/* Methods for square */
-func (s square) area() float64 {
-	return s.length * s.length
+func (d Dog) Walk() string {
+	return "walk"
 }
 
-func (s square) circumf() float64 {
-	return s.length * 4
+type Cat struct{}
+
+func (c Cat) Speak() string {
+	return "Meow"
 }
 
-/* struct circle */
-type circle struct {
-	radius float64
+func (c Cat) Walk() string {
+	return "Meow"
 }
 
-/* Methods for circle */
-func (c circle) area() float64 {
-	return math.Pi * c.radius * c.radius
-}
-
-func (c circle) circumf() float64 {
-	return 2 * math.Pi * c.radius
-}
-
-func printShapeInfo(s shape){
-	fmt.Printf("Area of %T is: %0.2f \n", s, s.area())
-	fmt.Printf("Circumference of %T is: %0.2f \n", s, s.circumf())
-}
-
-/* Main method */
 func main() {
-	shapes := []shape {
-		square{length: 15.2},
-		circle{radius: 12.3},
-	}
-
-	for _,v := range shapes {
-		printShapeInfo(v)
-		fmt.Println("---")
+	animals := []Animal{Dog{}, Cat{}}
+	for _, animal := range animals {
+		fmt.Println(animal.Speak())
 	}
 }
