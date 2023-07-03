e3fcfc56b74fa3ddc269cd8e42e4183be7fc9a58
prasad-nimap <prasadjunghare@nimapinfotech.com>
2023-07-03 13:01:04 +0530

    updated code and updated readme for arrays tut

---
+++ Golang-Tutorials/Arrays/README.md
@@ -0,0 +1,15 @@
+## Arrays
+
+Arrays are used to store multiple values of same type in a single variable.
+
+## Declaring an array
+
+```
+var array_name = []int{1,2,3,4,5}
+```
+
+## Declaring an array using := operator
+
+```
+array_name := []int{1,2,3,4,5}
+```
--- Golang-Tutorials/Arrays/arrays.go
+++ Golang-Tutorials/Arrays/arrays.go
@@ -3,15 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	/*
-		1. with var keyword
-			var array_name = [length]datatype{values} // here length is defined
-			var array_name = [...]datatype{values} // here length is inferred
-
-		2. with warlus operator
-			array_name := [length]datatype{values}
-			array_name := [...]datatype{values}
-	*/
 
 	my_arr := [...]int{1, 2, 3, 4, 5}
 
