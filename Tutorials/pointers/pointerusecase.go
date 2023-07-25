85c6d7d77fbb2e044c3eb43c8d326bd1410c4d2d
prasad-nimap <prasadjunghare@nimapinfotech.com>
2023-07-25 11:59:13 +0530

    added comments to real usecase pointer example

--- Tutorials/pointers/pointerusecase.go
+++ Tutorials/pointers/pointerusecase.go
@@ -11,12 +11,16 @@ type Point struct {
 }
 
 type Tank struct {
-	Name     string   `json:"name"`
+	Name string `json:"name"`
+
+	// Inherit struct Point (In golang iits called composition)
 	Position Point    `json:"position"`
+
 	Drivers  []string `json:"drivers"`
 }
 
 func main() {
+	// initializing the values for Tank struct
 	myTank := Tank{
 		Name: "tank_1234",
 		Position: Point{
@@ -26,10 +30,13 @@ func main() {
 		Drivers: []string{"man_1", "man_2"},
 	}
 
+	// Return json encoding of myTank
 	jsondata, err := json.Marshal(&myTank)
 	if err != nil {
 		fmt.Println("err", err)
 	}
+
+	// Print the output
 	fmt.Println(string(jsondata))
 
 	/* var newTank Tank
