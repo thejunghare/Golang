7bea28c85afaba991505d717b9b322e0bd11b7f2
prasad-nimap <prasadjunghare@nimapinfotech.com>
2023-07-25 10:41:02 +0530

    renamed the main folder to Problems and Tutorials

---
+++ Tutorials/Maps/README.md
@@ -0,0 +1,12 @@
+## Maps
+
+Maps are used to store data values in key:value pairs.
+
+```
+map_name := map[keyType]valueType{Key1:value1, Key2:value2,...}
+```
+
+- Create a map using key function
+```
+map_name = make(map[keyType]valueType)
+```
--- Tutorials/Maps/maps.go
+++ Tutorials/Maps/maps.go
@@ -97,18 +97,10 @@ type employee struct {
 }
 
 func main() {
-	/*
-		Maps are used to store data values in key:value pairs.
-		syntax -> map_name := map[keyType]valueType{Key1:value1, Key2:value2,...}
-	*/
-
+	
 	/* bike := map[string]string{"Brand": "Honda", "Model": "cb shine"}
 	fmt.Println(bike) */
 
-	/*
-		Create a map using key function
-		map_name = make(map[keyType]valueType)
-	*/
 
 	my_map := make(map[string]string) // my_map is empty ->  make function is the correct way to create an empty map
 	my_map["brand"] = "Honda"         // my_map is no longer empty
